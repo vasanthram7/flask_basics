2fe8841dba605999c6f45f829c3a7fcdd3f78a33
vasanthram7 <vramsjec@gmail.com>
2019-04-26 12:48:58 +0530

    second commit

---
+++ class.py
@@ -0,0 +1,8 @@
+class Math:
+    notes = 'this is class variable'
+
+    def add(self, num1, num2):
+        print('addition:', num1 + num2)
+
+    def multiply(self, num1, num2):
+        print('multiplication:', num1, num2)
